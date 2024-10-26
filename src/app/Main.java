d63db888a1fdc2a504ae74b97518d6659a37e10b
vladmyndru <ladvis62@gmail.com>
2024-10-26 18:10:40 +0300

    add converter kilometers to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,12 +9,19 @@ public class Main {
 
 
         double miles = 20;
+        double km = 20;
         double kilometers = milesToKilometers(miles);
+        double mls = kilometersToMiles(km);
         System.out.println("Result is: " + miles + " miles is " + kilometers +
                 " kilometers");
+        System.out.println("Result is: " + km + " kilometers is " + mls +
+                " miles.") ;
     }
 
     public static double milesToKilometers (double miles) {
         return miles * 1.60934;
+    }
+    public static double kilometersToMiles (double km) {
+        return km / 1.60934;
     }
 }
