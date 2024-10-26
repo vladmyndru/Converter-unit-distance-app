2ee9bc2eb4028435102aa3984ae4f3d66a8e0afb
vladmyndru <ladvis62@gmail.com>
2024-10-26 17:49:35 +0300

    Add convertation miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter unit distance");
+
+
+        double miles = 20;
+        double kilometers = milesToKilometers(miles);
+        System.out.println("Result is: " + miles + " miles is " + kilometers +
+                " kilometers");
+    }
+
+    public static double milesToKilometers (double miles) {
+        return miles * 1.60934;
     }
 }
